2534835143b5f2bb2badab8cccfbcbf6e7279535
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2022-10-07 08:36:29 +0200

    simplevers1

--- src/funcs.py
+++ src/funcs.py
@@ -1,24 +1,29 @@
 """Exercises with simple functions"""
-
+from math import sqrt
 
 def prod(a, b, c):
     """
     Compute the product of three numbers.
 
     >>> prod(1, 2, 3)
-    'TEST ME!'
+    6
     """
-    ...
+    product = a*b*c
+    return product
 
+glob_val = 4
 
 def prod2(b):
     """
     Get a global a and write to a local c before computing prod(a, b, c)
 
     >>> prod2(42)
-    'TEST ME'
+    a*42*c
     """
-    ...
+    a = glob_val
+    c = 2
+    product = a*b*c
+    return product
 
 
 def longest(x, y):
@@ -26,9 +31,16 @@ def longest(x, y):
     Returning the longest of two lists.
 
     >>> longest([1, 2, 3], [4, 5])
-    'TEST ME'
+    [1, 2, 3]
     """
-    ...
+    if len(x) > len(y):
+        return x
+    elif len(y) > len(x):
+        return y
+    else:
+        return None
+
+    # return x if len(x) > len(y) else y
 
 
 def dist(p1, p2):
@@ -36,8 +48,13 @@ def dist(p1, p2):
     Compute the distance between p1 and p2.
 
     >>> dist((1,2), (3,4))
-    'TEST ME'
+    sqrt(8)
     """
     x1, y1 = p1
     x2, y2 = p2
-    ...
+    
+    d = sqrt(abs((x1-x2))**2+abs((y1-y2))**2)
+
+    return d
+
+
--- src/test_funcs.py
+++ src/test_funcs.py
@@ -1,6 +1,7 @@
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
+from math import sqrt
 
 from funcs import (
     prod,
@@ -11,16 +12,16 @@ from funcs import (
 
 
 def test_prod():
-    assert prod(1, 2, 3) == 'TEST ME'
+    assert prod(1, 2, 3) == 6
 
 
 def test_prod2():
-    assert prod2(2) == 'TEST ME'
+    assert prod2(2) == 16
 
 
 def test_longest():
-    assert longest([1, 2, 3], [4, 5]) == 'TEST ME'
+    assert longest([1, 2, 3], [4, 5]) == [1, 2, 3]
 
 
 def test_dist():
-    assert dist((1, 2), (3, 4)) == 'TEST ME'
+    assert dist((1, 2), (3, 4)) == sqrt(8)
